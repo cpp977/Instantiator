3fd1f7b876ce543f4d0e51587cd7096b94db0286
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2021-10-19 12:25:09 +0200

    Implemented intelligent loop to also inject nested explicit instantiations which only appear if some instantitations are already injected.

--- Instantiator.cpp
+++ Instantiator.cpp
@@ -1,7 +1,7 @@
-// Declares clang::SyntaxOnlyAction.
 #include <iostream>
 #include <ranges>
 #include <sstream>
+#include <unordered_set>
 
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -22,17 +22,13 @@
 #include "Callbacks/GetNeededInstantiations.hpp"
 #include "Callbacks/InjectInstantiation.hpp"
 
-using namespace clang;
 using namespace clang::ast_matchers;
 
-DeclarationMatcher TemplateInstantiationMatcher =
+clang::ast_matchers::DeclarationMatcher TemplateInstantiationMatcher =
     functionDecl(isTemplateInstantiation(), unless(isDefinition()), unless(matchesName("std::")), unless(matchesName("__gnu_cxx::")))
         .bind("templ_func_instantation");
 
-DeclarationMatcher FunctionDefMatcher = functionDecl(isDefinition()).bind("func_definition");
-
-using namespace clang::tooling;
-using namespace llvm;
+clang::ast_matchers::DeclarationMatcher FunctionDefMatcher = functionDecl(isDefinition()).bind("func_definition");
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
@@ -41,20 +37,20 @@ static llvm::cl::OptionCategory MyToolCategory("Instantiator options");
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...\n");
+static llvm::cl::extrahelp MoreHelp("\nMore help text...\n");
 
 int main(int argc, const char** argv)
 {
-    auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory, llvm::cl::OneOrMore);
+    auto ExpectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, MyToolCategory, llvm::cl::OneOrMore);
     if(!ExpectedParser) {
         // Fail gracefully for unsupported options.
         llvm::errs() << ExpectedParser.takeError();
         return 1;
     }
-    CommonOptionsParser& OptionsParser = ExpectedParser.get();
+    clang::tooling::CommonOptionsParser& OptionsParser = ExpectedParser.get();
 
     std::vector<std::string> main_and_injection_files = OptionsParser.getCompilations().getAllFiles();
 
@@ -66,40 +62,67 @@ int main(int argc, const char** argv)
     for(const auto& source : main_and_injection_files) { std::cout << '\t' << "-- " << source << std::endl; }
     llvm::ArrayRef<std::string> sources(main_and_injection_files.data(), main_and_injection_files.size());
 
-    ClangTool Tool(OptionsParser.getCompilations(), sources);
+    clang::tooling::ClangTool Tool(OptionsParser.getCompilations(), sources);
 
-    std::vector<std::unique_ptr<ASTUnit>> allASTs;
+    std::vector<std::unique_ptr<clang::ASTUnit>> allASTs;
     std::vector<Injection> toDoList;
     [[maybe_unused]] int success = Tool.buildASTs(allASTs);
-    // TemplatePrinter Printer;
+    std::map<std::string, std::size_t> file2AST;
+    for(std::size_t i = 0; i < allASTs.size(); i++) { file2AST.insert(std::make_pair(main_and_injection_files[i], i)); }
+
     GetNeededInstantiations Getter;
 
     Getter.toDoList = &toDoList;
-    MatchFinder Finder;
-    Finder.addMatcher(TemplateInstantiationMatcher, &Getter);
+    clang::ast_matchers::MatchFinder Finder;
+    Finder.addMatcher(/*Matcher*/ TemplateInstantiationMatcher, /*Callback*/ &Getter);
 
     // match in current main file.
-    Finder.matchAST(allASTs[0]->getASTContext());
-    std::cout << "**************************************************" << std::endl;
-    std::cout << "Run on main file produced the following ToDo-List:" << std::endl;
-    std::cout << "**************************************************" << std::endl;
-    for(const auto& toDo : toDoList) { std::cout << toDo << std::endl; }
-    std::cout << std::endl;
-    for(std::size_t i = 1; i < allASTs.size(); i++) {
-        // const auto SM = allASTs[i]->getSourceManager();
-        std::cout << "*****************************************************************************************************************" << std::endl;
-        std::cout << "Search in AST of file "
-                  << allASTs[i]->getSourceManager().getFileEntryForID(allASTs[i]->getSourceManager().getMainFileID())->getName().str() << std::endl;
-        std::cout << "*****************************************************************************************************************" << std::endl;
-        Rewriter rewriter(allASTs[i]->getSourceManager(), allASTs[i]->getLangOpts());
-        MatchFinder FuncFinder;
-        InjectInstantiation instantiator;
-        instantiator.toDoList = &toDoList;
-        instantiator.rewriter = &rewriter;
-        FuncFinder.addMatcher(/*Matcher*/ FunctionDefMatcher, /*Callback*/ &instantiator);
-        FuncFinder.matchAST(allASTs[i]->getASTContext());
-        std::cout << std::endl;
-        rewriter.overwriteChangedFiles();
+    std::unordered_set<std::string> work_list;
+    work_list.insert(main_and_injection_files[0]);
+
+    while(work_list.size() > 0) {
+        auto copyOf_work_list = work_list;
+        for(const auto& item : copyOf_work_list) {
+            work_list.erase(item);
+            Finder.matchAST(allASTs[file2AST[item]]->getASTContext());
+            std::cout << "*****************************************************************************************************************"
+                      << std::endl;
+            std::cout << "Run on file " << item << " produced the following ToDo-List:" << std::endl;
+            std::cout << "*****************************************************************************************************************"
+                      << std::endl;
+            for(const auto& toDo : toDoList) { std::cout << toDo << std::endl; }
+            std::cout << std::endl;
+            for(const auto& file_for_search : main_and_injection_files) {
+                if(file_for_search == item) { continue; }
+                std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+                          << std::endl;
+                std::cout << "Search in AST of file " << file_for_search << "=="
+                          << allASTs[file2AST[file_for_search]]
+                                 ->getSourceManager()
+                                 .getFileEntryForID(allASTs[file2AST[file_for_search]]->getSourceManager().getMainFileID())
+                                 ->getName()
+                                 .str()
+                          << std::endl;
+                std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+                          << std::endl;
+                clang::Rewriter rewriter(allASTs[file2AST[file_for_search]]->getSourceManager(), allASTs[file2AST[file_for_search]]->getLangOpts());
+                clang::ast_matchers::MatchFinder FuncFinder;
+                InjectInstantiation instantiator;
+                instantiator.toDoList = &toDoList;
+                instantiator.rewriter = &rewriter;
+                FuncFinder.addMatcher(/*Matcher*/ FunctionDefMatcher, /*Callback*/ &instantiator);
+                FuncFinder.matchAST(allASTs[file2AST[file_for_search]]->getASTContext());
+                std::cout << std::endl;
+                rewriter.overwriteChangedFiles();
+                bool HAS_INJECTED_INTANTIATION = rewriter.buffer_begin() != rewriter.buffer_end();
+                if(HAS_INJECTED_INTANTIATION) {
+                    work_list.insert(file_for_search);
+                    auto PCHContainerOps = std::make_shared<clang::PCHContainerOperations>();
+                    bool AST_NOT_UPDATED = allASTs[file2AST[file_for_search]]->Reparse(PCHContainerOps);
+                    if(AST_NOT_UPDATED) { std::cerr << "Error while reparsinf the AST" << std::endl; }
+                }
+            }
+        }
     }
     std::cout << "#toDos that are left: " << toDoList.size() << std::endl;
 }
