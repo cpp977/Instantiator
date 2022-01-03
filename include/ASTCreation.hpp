e22fc105b4080d1a58b58425613d88b4f15b2cf6
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-01-03 16:03:28 +0100

    Added include guard.

--- include/ASTCreation.hpp
+++ include/ASTCreation.hpp
@@ -1,6 +1,12 @@
+#ifndef AST_CREATION_H_
+#define AST_CREATION_H_
+
+#include "clang/Tooling/Tooling.h"
 
 int parseOrLoadAST(std::unique_ptr<clang::ASTUnit>& AST, const clang::tooling::CompilationDatabase& db, const std::string filename);
 
 namespace internal {
 bool is_cached(const clang::tooling::CompilationDatabase& db, std::string filename);
 }
+
+#endif
