ea499db0b61c8a313f87911e1a1243a2acb7e7d2
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-01-05 14:10:12 +0100

    Added documentation.

--- include/ASTCreation.hpp
+++ include/ASTCreation.hpp
@@ -6,7 +6,15 @@
 int parseOrLoadAST(std::unique_ptr<clang::ASTUnit>& AST, const clang::tooling::CompilationDatabase& db, const std::string filename);
 
 namespace internal {
+/**
+ * Helper function to detect whether the AST for \p filename is cached on disk.
+ * \return `true` if the AST is cached, `false` if not.
+ *
+ * \param db : [clang::tooling::CompilationDatabase](https://clang.llvm.org/doxygen/classclang_1_1tooling_1_1CompilationDatabase.html) to be able to
+ *             invoke the compiler for getting the dependencies.
+ * \param filename : Name of the source file
+ */
 bool is_cached(const clang::tooling::CompilationDatabase& db, std::string filename);
-}
+} // namespace internal
 
 #endif
