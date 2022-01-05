20dbadde5056c44f0773501ff20fb0d959e251f4
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-01-05 14:12:15 +0100

    Internal functions for parsing information AST nodes.

---
+++ include/Parsing.hpp
@@ -0,0 +1,42 @@
+#ifndef PARSING_H_
+#define PARSING_H_
+
+#include "clang/AST/PrettyPrinter.h"
+
+#include "Param.hpp"
+
+namespace clang {
+class TemplateArgumentList;
+class ParmVarDecl;
+} // namespace clang
+
+/** \file Parsing.hpp
+ *  \brief Helper functions for parsing information from AST nodes.
+ */
+
+/**
+ * Namespace for internal functions.
+ */
+namespace internal {
+/**
+ * Helper function to load template arguments.
+ * \return `std::vector<std::string>` with the names of all types of template arguments.
+ * \param TAL Pointer to a [clang::TemplateArgumentList](https://clang.llvm.org/doxygen/classclang_1_1TemplateArgumentList.html).
+ * \param pp [clang::PrintingPolicy](https://clang.llvm.org/doxygen/structclang_1_1PrintingPolicy.html) which controls how strings are created.
+ *
+ * \note Template arguments can have different kinds (types, integral values, packs, template template).
+ *       The function returns the type names for typed template parameters and the values for integral parameters.
+ */
+std::vector<std::string> parseTemplateArgs(const clang::TemplateArgumentList* TAL, clang::PrintingPolicy pp);
+
+/**
+ * Helper function to load function parameters.
+ * \return `std::vector<Param>` with an instance of Param for each parameter.
+ *
+ * \param Args Array of [clang::ParmVarDecl](https://clang.llvm.org/doxygen/classclang_1_1ParmVarDecl.html).
+ * \param pp [clang::PrintingPolicy](https://clang.llvm.org/doxygen/structclang_1_1PrintingPolicy.html) which controls how strings are created.
+ */
+std::vector<Param> parseFunctionArgs(llvm::ArrayRef<clang::ParmVarDecl*> Args, clang::PrintingPolicy pp);
+} // namespace internal
+
+#endif
---
+++ src/Parsing.cpp
@@ -0,0 +1,65 @@
+#include "Parsing.hpp"
+
+#include "clang/AST/DeclTemplate.h"
+#include "clang/AST/TemplateName.h"
+
+namespace internal {
+std::vector<std::string> parseTemplateArgs(const clang::TemplateArgumentList* TAL, clang::PrintingPolicy pp)
+{
+    std::vector<std::string> out;
+    if(TAL) {
+        out.resize(TAL->size());
+        for(std::size_t i = 0; i < TAL->size(); i++) {
+            switch(TAL->get(i).getKind()) {
+            case clang::TemplateArgument::ArgKind::Type: {
+                out[i] = TAL->get(i).getAsType().getAsString(pp);
+                break;
+            }
+            case clang::TemplateArgument::ArgKind::Integral: {
+                llvm::SmallString<10> name;
+                TAL->get(i).getAsIntegral().toString(name);
+                out[i] = name.str().str();
+                break;
+            }
+            case clang::TemplateArgument::ArgKind::Pack: {
+                out.resize(out.size() + TAL->get(i).pack_size() - 1);
+                for(auto pack_it = TAL->get(i).pack_begin(); pack_it != TAL->get(i).pack_end(); pack_it++) {
+                    switch(pack_it->getKind()) {
+                    case clang::TemplateArgument::ArgKind::Type: {
+                        out[i + std::distance(TAL->get(i).pack_begin(), pack_it)] = pack_it->getAsType().getAsString(pp);
+                        break;
+                    }
+                    case clang::TemplateArgument::ArgKind::Integral: {
+                        llvm::SmallString<10> name;
+                        pack_it->getAsIntegral().toString(name);
+                        out[i + std::distance(TAL->get(i).pack_begin(), pack_it)] = name.str().str();
+                        break;
+                    }
+                    }
+                }
+                break;
+            }
+            case clang::TemplateArgument::ArgKind::Template: {
+                llvm::raw_string_ostream OS(out[i]);
+#if INSTANTIATOR_LLVM_MAJOR > 13
+                TAL->get(i).getAsTemplate().print(OS, pp, clang::TemplateName::Qualified::Fully);
+#else
+                TAL->get(i).getAsTemplate().print(OS, pp, false);
+#endif
+                OS.str();
+                break;
+            }
+            }
+        }
+    }
+    return out;
+}
+
+std::vector<Param> parseFunctionArgs(llvm::ArrayRef<clang::ParmVarDecl*> Args, clang::PrintingPolicy pp)
+{
+    std::vector<Param> out;
+    out.resize(Args.size());
+    for(auto it = Args.begin(); it != Args.end(); it++) { out[std::distance(Args.begin(), it)] = Param::createFromParmVarDecl(*it, pp); }
+    return out;
+}
+} // namespace internal
