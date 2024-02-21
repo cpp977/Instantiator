fb321dde63e83d40474f2e1b78d0f4b4c0ed55d5
Matthias Peschke <peschke@dfn-cert.de>
2024-02-21 18:26:47 +0100

    Improved test script.

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -1,9 +1,6 @@
 #!/usr/bin/env bats
 
 setup_file() {
-    load '/usr/lib/bats/bats-support/load.bash'
-    load '/usr/lib/bats/bats-assert/load.bash'
-
     # get the containing directory of this file
     SCRIPT_DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
     export SCRIPT_DIR
@@ -17,26 +14,37 @@ teardown_file() {
     rm -rf "${SCRIPT_DIR}/build"
 }
 
+setup() {
+    load '/usr/lib/bats/bats-support/load.bash'
+    load '/usr/lib/bats/bats-assert/load.bash'
+}
+
 @test "Configure the test project with CMake." {
     run cmake -S "${SCRIPT_DIR}/Calculator/" -B "${SCRIPT_DIR}/build"
+    assert_success
 }
 
 @test "Run Instantiator to generate template instantantions." {
     run Instantiator -p "${SCRIPT_DIR}/build/compile_commands.json" "${SCRIPT_DIR}/Calculator/src/Calculator.cpp"
+    assert_success
 }
 
 @test "Build the executable." {
-    cmake --build "${SCRIPT_DIR}/build"
+    run cmake --build "${SCRIPT_DIR}/build"
+    assert_success
 }
 
 @test "Run Instantiator to remove template instantantions." {
-    Instantiator -p "${SCRIPT_DIR}/build/compile_commands.json" --clean "${SCRIPT_DIR}/Calculator/src/Calculator.cpp"
+    run Instantiator -p "${SCRIPT_DIR}/build/compile_commands.json" --clean "${SCRIPT_DIR}/Calculator/src/Calculator.cpp"
+    assert_success
 }
 
 @test "Reformat the codebase with clang-format." {
-    find "${SCRIPT_DIR}" -iname "*.hpp" -o -iname "*.cpp" -print0 | xargs -0 clang-format -i
+    run bash -c "find "${SCRIPT_DIR}" -iname "*.hpp" -o -iname "*.cpp" -print0 | xargs -0 clang-format -i"
+    assert_success
 }
 
 @test "Run git diff to check that the original version of code is restored." {
-    git --no-pager -C "${SCRIPT_DIR}/.." diff "${SCRIPT_DIR}/Math"
+    run git --no-pager -C "${SCRIPT_DIR}/.." diff --quiet "${SCRIPT_DIR}/Math"
+    assert_success
 }
