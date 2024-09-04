ca161fe4a218368a534bc49282abed63f05789b3
Matthias Peschke <peschke@dfn-cert.de>
2024-09-04 17:29:48 +0200

    Fixed bug in sanitizer module.

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,6 +1,7 @@
 function(enable_sanitizers project_name)
 
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES
+                                             ".*Clang")
 
     if(INSTANTIATOR_ENABLE_COVERAGE)
       target_compile_options(${project_name} INTERFACE --coverage)
@@ -23,37 +24,40 @@ function(enable_sanitizers project_name)
 
     if(INSTANTIATOR_ENABLE_SANITIZER_THREAD)
       if("address" IN_LIST SANITIZERS OR "leak" IN_LIST SANITIZERS)
-        message(WARNING "Thread sanitizer does not work with Address and Leak sanitizer enabled")
+        message(
+          WARNING
+            "Thread sanitizer does not work with Address and Leak sanitizer enabled"
+        )
       else()
         list(APPEND SANITIZERS "thread")
       endif()
     endif()
 
-    if(INSTANTIATOR_ENABLE_SANITIZER_MEMORY AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    if(INSTANTIATOR_ENABLE_SANITIZER_MEMORY AND CMAKE_CXX_COMPILER_ID MATCHES
+                                                ".*Clang")
       if("address" IN_LIST SANITIZERS
          OR "thread" IN_LIST SANITIZERS
          OR "leak" IN_LIST SANITIZERS)
-        message(WARNING "Memory sanitizer does not work with Address, Thread and Leak sanitizer enabled")
+        message(
+          WARNING
+            "Memory sanitizer does not work with Address, Thread and Leak sanitizer enabled"
+        )
       else()
         list(APPEND SANITIZERS "memory")
       endif()
     endif()
 
-    list(
-      JOIN
-      SANITIZERS
-      ","
-      LIST_OF_SANITIZERS)
+    list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
 
   endif()
 
   if(LIST_OF_SANITIZERS)
-    if(NOT
-       "${LIST_OF_SANITIZERS}"
-       STREQUAL
-       "")
-      target_compile_options(${project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
-      target_link_libraries(${project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+    if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
+      message(WARNING "${LIST_OF_SANITIZERS}")
+      target_compile_options(${project_name}
+                             PRIVATE -fsanitize=${LIST_OF_SANITIZERS})
+      target_link_libraries(${project_name}
+                            PRIVATE -fsanitize=${LIST_OF_SANITIZERS})
     endif()
   endif()
 
